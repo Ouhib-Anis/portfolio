9a841ecdf98be1fcb7c0adee1efefce31c60ceb2
Ouhib-Anis <Anisdz570@gmail.com>
2024-07-28 21:54:46 +0200

    modification de code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <section id="home" className="home">
       <img src={profileImage} alt="Profil" className="profile-image" />
       <h1>Bienvenue sur mon Portfolio</h1>
-      <p>Je suis Ouhib Anis, un développeur passionné.</p>
+     
     </section>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,12 +9,22 @@ const Projects = () => {
       <div className="project-list">
         <div className="project-item">
           <h3>Projet 1</h3>
-          <p>Description du projet 1.</p>
+          <p>Élaboration d’un outil pédagogique pour la simulation de gestion des transactions dans une base de données en utilisant java swing</p>
         </div>
         <div className="project-item">
           <h3>Projet 2</h3>
-          <p>Description du projet 2.</p>
+          <p>Conception et réalisation d’une application web de gestion des charges pédagogique des enseignants en utilisant html, css, js, PHP, MySQL</p>
         </div>
+
+        <div className="project-item">
+          <h3>Projet 3</h3>
+          <p>Développement de la gestion des formations dans un établissement RH avec Odoo 16</p>
+        </div>
+        <div className="project-item">
+          <h3>Projet 4</h3>
+          <p>Personnalisation de la gestion commerciale avec Odoo 16</p>
+        </div>
+        
         {/* Ajoutez d'autres projets ici */}
       </div>
     </section>
